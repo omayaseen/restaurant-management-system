1512231b99638f53bf8c6b5cb74adec8166fd03d
OMA Yaseen <omayaseen@OMAs-MacBook-Pro.local>
2026-08-26 02:49:15 +0530

    Serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('orders/',include('orders.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
